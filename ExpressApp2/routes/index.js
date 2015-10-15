998c4bf586ee4d1e66d2a73608aae9def9cec6fe
norofolknchance <chamberlain_adam@hotmail.com>
2015-10-15 15:20:46 +0100

    .

--- ExpressApp2/routes/index.js
+++ ExpressApp2/routes/index.js
@@ -2,7 +2,7 @@
 // routes
 
 exports.index = function (req, res) {
-    res.render('index');
+    res.render('index',{ title: 'My First Express App', year: new Date().getFullYear() });
 };
 
 exports.partials = function (req, res) {
